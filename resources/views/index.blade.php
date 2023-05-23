69994cf8a6a18a5a11e5874739f6471550ccc52b
TijsVsN <132552284+TijsVsN@users.noreply.github.com>
2023-05-23 17:56:53 +0200

    Made a basic UI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,22 +3,36 @@
 @section('content')
 
 {{--Main layout --}}
-<div class="w-full flex">
-
-{{--Settings tab--}}
-<div class=" w-72 h-full border border-black">
-<h1 class="font-bold text-base ml-5">Settings</h1>
-<div>
-    
-</div>
-</div>
+<div class="w-full h-screen">
 
 {{--Map--}}
-<div>
+<div class="h-4/6">
+<div id="map" class="h-full"></div>
+<script>
+    var map = L.map('map').setView([51.505, -0.09], 13);
+    
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    map.invalidateSize();
+
+    </script>
+</div>
+
+{{--Settings tab--}}
+<div class="border border-black w-full h-fit pl-5">
+    <h1 class="font-bold text-base">Settings</h1>
+    <div>
+        <p>This is the settings tab. Here you will upload your path, and adjust settings.</p>
+    </div>
+    </div>
+
 
 </div>
 
-</div>
+
 
 
 @endsection
+
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,5 +1,5 @@
 
-@vite('resources/css/app.css')
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +7,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OpenSoaringAnalyzer</title>
+
+    
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 </head>
+@vite('resources/css/app.css')
 <body>
 
     <div class="w-full">
